b077995295fd7a3c1c440bf5f160ffbdaf4dc455
gcskoenig <gunnar.koenig@pm.me>
2020-09-17 16:18:46 +0200

    refined class and method signatures and logic

--- _explainer.py
+++ _explainer.py
@@ -6,34 +6,67 @@ More details in the docstring for the class Explainer.
 
 
 class Explainer():
-	"""Uses Relative Feature Importance to compute the importance of features
-	relative to any set of features G.
+    """Uses Relative Feature Importance to compute the importance of features
+    relative to any set of features G.
 
-	Default samplers or loss function can be defined.
-	Masks allows to specify for which features importance
-	shall be computed.
+    Default samplers or loss function can be defined.
+    Masks allows to specify for which features importance
+    shall be computed.
 
-	Attributes:
-		model: Model or predict function.
-		mask: Features to be explained.
-		sampler: default sampler.
-		loss: default loss.
-	"""
-	def __init__(self, model, mask, sampler=None, loss=None):
-		"""Inits Explainer with model, mask and potentially sampler and loss"""
-		self.model = model
-		self.mask = mask
-		self.sampler = sampler
-		self.loss = loss
+    Attributes:
+        model: Model or predict function.
+        fsoi: Features of interest.
+        X_train: Training data for Resampling.
+        sampler: default sampler.
+        loss: default loss.
+    """
 
+    def __init__(self, model, fsoi, X_train, sampler=None, loss=None):
+        """Inits Explainer with model, mask and potentially sampler and loss"""
+        self.model = model
+        self.fsoi = fsoi
+        self.X_train = X_train
+        self.loss = loss
 
-	def rfi(self, X_test, y_test, G, sampler=None, loss=None):
-		"""Computes Relative Feature importance
+    def rfi(self, X_test, y_test, G, sampler=None, loss=None):
+        """Computes Relative Feature importance
 
-		Args:
+        # TODO(gcsk): allow handing a sample as argument
+        #             (without needing sampler)
 
+        Args:
+            X_test: data to use for sampler training and evaluation.
+            y_test: labels for evaluation.
+            G: relative feature set
+            sampler: choice of sampler. Default None. Will throw an error
+              when sampler is None and self.sampler is None as well.
+            loss: choice of loss. Default None. Will throw an Error when
+              both loss and self.loss are None.
 
-		Returns:
+        Returns:
+            A np.array with the relative feature importance values for
+            features of interest.
+        """
+        if sampler is None:
+            if self.sampler is None:
+                # TODO(gcsk): raise Exception
+                pass
+            else:
+                sampler = self.sampler
+                pass
 
-		"""
-		pass
+        if loss is None:
+            if self.loss is None:
+                # TODO(gcsk): raise Exception
+                pass
+            else:
+                loss = self.loss
+                pass
+
+        # TODO(gcsk): check whether the sampler is trained on G
+        # TODO(gcsk): if the sampler is not traine on G, train it
+        # TODO(gcsk): if the sampler is trained
+
+        # TODO(gcsk): sample the replacements
+
+        # TODO(gcsk): RFI routine
--- _sampler.py
+++ _sampler.py
@@ -1,45 +1,68 @@
-"""Sampler can be used sample perturbed versions of a variable conditional on a set G.
+"""Sampler can be used sample perturbed versions of a variable
+conditional on a set G.
 
 More details can be found in the class description
 """
 
+
 class Sampler():
-	"""Can be used to resample perturbed versions of a variable conditional on
-	any set of variables G.
+    """Can be used to resample perturbed versions of a variable conditional on
+    any set of variables G.
 
-	More details here.
+    More details here.
 
-	Attributes:
-		X_train: reference to training data.
-		mask: features of interest.
-	"""
-	def __init__(self, X_train, mask):
-		"""Initialize Sampler with X_train and mask."""
-		self.X_train = X_train
-		self.mask = mask
+    Attributes:
+        X_train: reference to training data.
+        mask: features of interest.
+        __trained: private variable indicating whether the sampler was trained
+        __G: private variable indicating regarding which set G
+          the sampler was trained
+    """
 
-	def train(G):
-		"""Trains sampler using the training dataset to resample
-		relative to any variable set G.
+    def __init__(self, X_train, fsoi):
+        """Initialize Sampler with X_train and mask."""
+        self.X_train = X_train
+        self.fsoi = fsoi
+        self.__trained = False
+        self.__G = None
 
-		Args:
-			G: arbitrary set of variables.
+    def is_trained(self, G):
+        """Indicates whether the Sampler has been trained"""
+        # TODO(gcsk): validate whether trained
+        # TODO(gcsk): validate whether G and __G coincide
+        pass
 
-		Returns:
-			Nothing. Now the sample function can be used
-			to resample on seen or unseen data.
-		"""
-		pass
+    def get_G(self):
+        """Access the set G that the Sampler was trained on"""
+        return self.__G
 
+    def train(self, G):
+        """Trains sampler using the training dataset to resample
+        relative to any variable set G.
 
-	def sample(X_eval):
-		"""Sample unmasked variables using trained resampler.
+        Args:
+            G: arbitrary set of variables.
 
-		Args:
-			X_eval: Data for which sampling shall be performed.
+        Returns:
+            Nothing. Now the sample function can be used
+            to resample on seen or unseen data.
+        """
+        self.__trained = True
+        self.__G = G
+        pass
 
-		Returns:
-			Resampled data for the unmasked variables.
-			np.array with shape (X_eval.shape[0], # of unmasked features)
-		"""
-		pass
+    def sample(self, X_eval):
+        """Sample features of interest using trained resampler.
+
+        Args:
+            X_eval: Data for which sampling shall be performed.
+
+        Returns:
+            Resampled data for the features of interest.
+            np.array with shape (X_eval.shape[0], # features of interest)
+        """
+        if self.is_trained():
+            # TODO(gcsk): assert that it was trained on the correct set
+            pass
+        else:
+            pass
