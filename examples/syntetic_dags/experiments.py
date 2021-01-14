Add predictive model, add RFI evaluation, fix bugs with sampling while rfi calculation, misc utils

examples/syntetic_dags/experiments.py
@@ -2,7 +2,7 @@ import mlflow
 import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
-import logging
+import importlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -13,6 +13,7 @@ from rfi.backend.goodness_of_fit import *
 from rfi.backend.utils import flatten_dict
 from rfi.samplers.cnflow import CNFSampler
 from rfi.samplers.gaussian import GaussianSampler
+from rfi.utils import search_nonsorted, calculate_hash
 import rfi.explainers.explainer as explainer
 
 
@@ -22,6 +23,8 @@ logger = logging.getLogger(__name__)
 @hydra.main(config_path='configs', config_name='config.yaml')
 def main(args: DictConfig):
 
+    # Non-strict access to fields
+    OmegaConf.set_struct(args, False)
     logger.info(OmegaConf.to_yaml(args, resolve=True))
 
     # Data generator
@@ -36,6 +39,20 @@ def main(args: DictConfig):
     # Experiment tracking
     mlflow.set_tracking_uri(args.exp.mlflow_uri)
     mlflow.set_experiment(args.data_generator.sem_type)
+
+    # Checking if run exist
+    if args.exp.check_exisisting_hash:
+        args.hash = calculate_hash(args)
+
+        existing_runs = mlflow.search_runs(filter_string=f"params.hash = '{args.hash}'",
+                                           run_view_type=mlflow.tracking.client.ViewType.ACTIVE_ONLY,
+                                           experiment_ids=mlflow.get_experiment_by_name(args.data_generator.sem_type).experiment_id)
+        if len(existing_runs) > 0:
+            logger.info('Skipping existing run.')
+            return
+        else:
+            logger.info('No runs found - perfoming one.')
+
     mlflow.start_run()
     mlflow.log_params(flatten_dict(args))
 
@@ -44,15 +61,31 @@ def main(args: DictConfig):
     test_df.to_csv(hydra.utils.to_absolute_path(f'{mlflow.get_artifact_uri()}/test.csv'), index=False)
     sem.dag.plot_dag()
     plt.savefig(hydra.utils.to_absolute_path(f'{mlflow.get_artifact_uri()}/dag.png'))
-    df = pd.concat([train_df, test_df], keys=['train', 'test']).reset_index().drop(columns=['level_1'])
-    g = sns.pairplot(df, plot_kws={'alpha': 0.25}, hue='level_0')
-    g.fig.suptitle(sem.__class__.__name__)
-    plt.savefig(hydra.utils.to_absolute_path(f'{mlflow.get_artifact_uri()}/data.png'))
+    if len(dag.var_names) <= 20:
+        df = pd.concat([train_df, test_df], keys=['train', 'test']).reset_index().drop(columns=['level_1'])
+        g = sns.pairplot(df, plot_kws={'alpha': 0.25}, hue='level_0')
+        g.fig.suptitle(sem.__class__.__name__)
+        plt.savefig(hydra.utils.to_absolute_path(f'{mlflow.get_artifact_uri()}/data.png'))
 
     metrics = {}
 
     for var_ind, target_var in enumerate(dag.var_names):
 
+        # Considering all the variables for input
+        input_vars = [var for var in dag.var_names if var != target_var]
+        y_train, X_train = train_df.loc[:, target_var].values, train_df.loc[:, input_vars].values
+        y_test, X_test = test_df.loc[:, target_var].values, test_df.loc[:, input_vars].values
+
+        # Fitting predictive model
+        logger.info(f'Fitting predictive model for target = {target_var} and inputs {input_vars}')
+        model = instantiate(args.pred_model)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        risk_func = getattr(importlib.import_module(f'sklearn.metrics'), args.exp.rfi.risk)
+        risk = risk_func(y_test, y_pred)
+        rfi_results = {'test_loss': risk}
+
+        # Conditional goodness-of-fit
         if args.exp.conditioning_mode == 'true_parents':
             context_vars = sem.model[target_var]['parents']
         elif args.exp.conditioning_mode == 'all':
@@ -62,34 +95,62 @@ def main(args: DictConfig):
         else:
             raise NotImplementedError()
 
-        if len(context_vars) == 0:
-            continue  # Not evaluating unconditional estimators
+        gof_results = {}
+        if len(context_vars) > 0:
+            logger.info(f'Fitting CDE for target = {target_var} and inputs {input_vars}')
+            estimator = instantiate(args.estimator, context_size=len(context_vars))
+            if args.estimator.fit_params is None:
+                getattr(estimator, args.estimator.fit_method)(train_inputs=y_train, train_context=X_train)
+            else:
+                getattr(estimator, args.estimator.fit_method)(train_inputs=y_train, train_context=X_train,
+                                                              **args.estimator.fit_params)
+            gof_results = {
+                'gof/kld': conditional_kl_divergence(estimator, sem, target_var, context_vars, args.exp, test_df),
+                'gof/hd': conditional_hellinger_distance(estimator, sem, target_var, context_vars, args.exp, test_df),
+                'gof/jsd': conditional_js_divergence(estimator, sem, target_var, context_vars, args.exp, test_df),
+                'gof/log_lik': estimator.log_prob(inputs=test_df.loc[:, target_var].values,
+                                                  context=test_df.loc[:, context_vars].values).mean(),
+                'gof/context_size': len(context_vars)
+            }
+            mlflow.log_metrics(gof_results, step=var_ind)
 
-        # Estimator
-        estimator = instantiate(args.estimator, context_size=len(context_vars))
-        if args.estimator.fit_params is None:
-            getattr(estimator, args.estimator.fit_method)(train_inputs=train_df.loc[:, target_var].values,
-                                                          train_context=train_df.loc[:, context_vars].values)
-        else:
-            getattr(estimator, args.estimator.fit_method)(train_inputs=train_df.loc[:, target_var].values,
-                                                          train_context=train_df.loc[:, context_vars].values,
-                                                          **args.estimator.fit_params)
+        # Relative feature importance
+        sampler = instantiate(args.estimator.sampler, X_train=X_train, X_val=X_test)
 
+        # 1. G = MB(target_var), FoI = input_vars / MB(target_var)
+        G_vars = list(sem.get_markov_blanket(target_var))
+        fsoi_vars = [var for var in input_vars if var not in list(sem.get_markov_blanket(target_var))]
+        G = search_nonsorted(input_vars, G_vars)
+        fsoi = search_nonsorted(input_vars, fsoi_vars)
+        if len(fsoi) > 0:
+            test_log_probs = sampler.train(fsoi, G)
+            rfi_explainer = explainer.Explainer(model.predict, fsoi, X_train, sampler=sampler, loss=risk_func, fs_names=input_vars)
+            mb_explanation = rfi_explainer.rfi(X_test, y_test, G, nr_runs=args.exp.rfi.nr_runs)
+            rfi_results['rfi/mb_cond_size'] = len(G_vars)
+            rfi_results['rfi/mb_mean_rfi'] = mb_explanation.rfi_means().mean()
+            rfi_results['rfi/mb_mean_log_lik'] = np.mean(test_log_probs) if len(G_vars) > 0 else np.nan
 
-        # Computing metrics
-        results = {
-            'gof/kld': conditional_kl_divergence(estimator, sem, target_var, context_vars, args.exp, test_df),
-            'gof/hd': conditional_hellinger_distance(estimator, sem, target_var, context_vars, args.exp, test_df),
-            'gof/jsd': conditional_js_divergence(estimator, sem, target_var, context_vars, args.exp, test_df),
-            'gof/log_lik': estimator.log_prob(inputs=test_df.loc[:, target_var].values,
-                                              context=test_df.loc[:, context_vars].values).mean()
-        }
-        mlflow.log_metrics(results, step=var_ind)
-        metrics = {k: metrics.get(k, []) + [results[k]] for k in set(list(metrics.keys()) + list(results.keys()))}
+        # 2. G = input_vars / MB(target_var), FoI = MB(target_var)
+        fsoi_vars = list(sem.get_markov_blanket(target_var))
+        G_vars = [var for var in input_vars if var not in list(sem.get_markov_blanket(target_var))]
+        G = search_nonsorted(input_vars, G_vars)
+        fsoi = search_nonsorted(input_vars, fsoi_vars)
+        if len(fsoi) > 0:
+            test_log_probs = sampler.train(fsoi, G)
+            rfi_explainer = explainer.Explainer(model.predict, fsoi, X_train, sampler=sampler, loss=risk_func, fs_names=input_vars)
+            non_mb_explanation = rfi_explainer.rfi(X_test, y_test, G, nr_runs=args.exp.rfi.nr_runs)
+            rfi_results['rfi/non_mb_cond_size'] = len(G_vars)
+            rfi_results['rfi/non_mb_mean_rfi'] = non_mb_explanation.rfi_means().mean()
+            rfi_results['rfi/non_mb_mean_log_lik'] = np.mean(test_log_probs) if len(G_vars) > 0 else np.nan
+
+        mlflow.log_metrics(rfi_results, step=var_ind)
+
+        results = {**rfi_results, **gof_results}
+        metrics = {k: metrics.get(k, []) + [results.get(k, np.nan)] for k in set(list(metrics.keys()) + list(results.keys()))}
+
 
     # Logging mean statistics
-    mlflow.log_metrics({k: np.mean(v) for (k, v) in metrics.items()}, step=len(dag.var_names))
-
+    mlflow.log_metrics({k: np.nanmean(v) for (k, v) in metrics.items()}, step=len(dag.var_names))
     mlflow.end_run()
 
 

rfi/backend/causality/sem.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 
 from rfi.backend.causality.dags import DirectedAcyclicGraph
 from rfi.backend.gaussian import GaussianConditionalEstimator
+from rfi.utils import search_nonsorted
 
 logger = logging.getLogger(__name__)
 
@@ -86,6 +87,7 @@ class StructuralEquationModel:
             else:
                 sample_shape = (1, size)
             self.model[node]['value'] = self.parents_conditional_sample(node, parent_values, sample_shape).reshape(-1)
+            assert (~self.model[node]['value'].isnan()).all()
         return torch.stack([self.model[node]['value'] for node in self.dag.var_names], dim=1)
 
     @staticmethod
@@ -247,11 +249,11 @@ class LinearGaussianNoiseSEM(StructuralEquationModel):
                 else np.random.uniform(*default_noise_std_bounds)
 
     def conditional_distribution(self, node: str, context: Dict[str, Tensor] = None) -> Distribution:
-        node_ind = np.searchsorted(self.dag.var_names, [node])
+        node_ind = search_nonsorted(self.dag.var_names, [node])
         if context is None or len(context) == 0:  # Unconditional distribution
             return Normal(self.joint_mean[node_ind].item(), torch.sqrt(self.joint_cov[node_ind, node_ind]).item())
         else:  # Conditional distribution
-            context_ind = np.searchsorted(self.dag.var_names, list(context.keys()))
+            context_ind = search_nonsorted(self.dag.var_names, list(context.keys()))
             cond_dist = GaussianConditionalEstimator()
             cond_dist.fit_mean_cov(self.joint_mean.numpy(), self.joint_cov.numpy(), inp_ind=node_ind, cont_ind=context_ind)
             context_sorted = [context[par_node] for par_node in context.keys()]

rfi/backend/cnf/cnf_estimator.py
@@ -148,7 +148,8 @@ class ConditionalNormalisingFlowEstimator(Flow):
             verbose=False,
             val_inputs: Union[np.array, Tensor] = None,
             val_context: Union[np.array, Tensor] = None,
-            log_frequency: int = 100):
+            log_frequency: int = 100,
+            **kwargs):
         """
         Method to fit Conditional Normalizing Flow density estimator
         Args:
@@ -208,7 +209,7 @@ class ConditionalNormalisingFlowEstimator(Flow):
         Returns: self
         """
 
-        ray.init(logging_level=logging.INFO)
+        ray.init(logging_level=logging.WARN)
         logger.info(f'Start fitting, using {n_splits}-fold split, time budget: {time_budget_s}')
 
         if hparam_grid is None:

rfi/backend/gaussian/gaussian_estimator.py
@@ -49,7 +49,7 @@ class GaussianConditionalEstimator(Distribution):
         if cont_ind.shape[0] == 1:
             Sigma_GG_inv = 1 / joint_cov[np.ix_(cont_ind, cont_ind)]
         else:
-            Sigma_GG_inv = np.linalg.pinv(joint_cov[np.ix_(cont_ind, cont_ind)], hermitian=True)
+            Sigma_GG_inv = np.linalg.pinv(joint_cov[np.ix_(cont_ind, cont_ind)])
         self.RegrCoeff = (joint_cov[np.ix_(inp_ind, cont_ind)] @ Sigma_GG_inv).reshape((len(inp_ind), len(cont_ind)))
         self.Sigma = joint_cov[np.ix_(inp_ind, inp_ind)] - self.RegrCoeff @ joint_cov[np.ix_(cont_ind, inp_ind)]
         # self.Sigma = cov_nearest(self.Sigma, threshold=1e-16)
@@ -86,8 +86,8 @@ class GaussianConditionalEstimator(Distribution):
         mu_part2 = self.RegrCoeff @ context.T
         for j in range(len(context)):
             mu = self.mu_part + mu_part2[:, j]
-            if len(self.inp_ind) == 1:
-                res[j, :, :] = np.random.normal(mu[0], np.sqrt(self.Sigma[0, 0]), num_samples)
-            else:
-                res[j, :, :] = np.random.multivariate_normal(mu, self.Sigma, num_samples)
+            # if len(self.inp_ind) == 1:
+            #     res[j, :, :] = np.random.normal(mu[0], np.sqrt(self.Sigma[0, 0]), num_samples)
+            # else:
+            res[j, :, :] = np.random.multivariate_normal(mu, self.Sigma, num_samples)
         return res

rfi/examples/__init__.py
@@ -6,6 +6,7 @@ import pandas as pd
 import logging
 
 from rfi.backend.causality.sem import StructuralEquationModel
+from rfi.utils import search_nonsorted
 
 logger = logging.getLogger(__name__)
 
@@ -33,8 +34,8 @@ class SyntheticExample:
         train = self.sem.sample(n_train, seed=train_seed).numpy()
         test = self.sem.sample(n_test, seed=test_seed).numpy()
 
-        inputs_ind = np.searchsorted(self.var_names, context_vars)
-        target_ind = np.searchsorted(self.var_names, target_var)
+        inputs_ind = search_nonsorted(self.var_names, context_vars)
+        target_ind = search_nonsorted(self.var_names, target_var)
         if not as_dataframes:
             return train[:, inputs_ind], train[:, target_ind], test[:, inputs_ind], test[:, target_ind]
         else:

rfi/explainers/explainer.py
@@ -79,26 +79,21 @@ class Explainer():
             raise RuntimeError('Sampler is not trained.')
         else:
             logging.info('Sampler is already trained.')
-
-        # sample perturbed
-        perturbed_foiss = np.zeros((self.fsoi.shape[0], nr_runs,
-                                   X_test.shape[0]))
  
         # TODO(gcsk): assess: does the sampler return the same sample every time?
-        # returns sample (#obs, #nr_runs, #fsoi)
-        sample = sampler.sample(X_test, self.fsoi, G, num_samples=nr_runs)
+        perturbed_foiss = sampler.sample(X_test, self.fsoi, G, num_samples=nr_runs).reshape(X_test.shape[0], nr_runs, self.fsoi.shape[0])
         
         lss = np.zeros((self.fsoi.shape[0], nr_runs, X_test.shape[0]))
 
         # compute observasitonwise loss differences for all runs and fois
-        for jj in np.arange(0, self.fsoi.shape[0], 1):
+        for jj_ind, jj in enumerate(self.fsoi):
             # copy of the data where perturbed variables are copied into
             X_test_perturbed = np.array(X_test)
             for kk in np.arange(0, nr_runs, 1):
                 # replaced with perturbed
-                X_test_perturbed[:, jj] = perturbed_foiss[jj, kk, :]
+                X_test_perturbed[:, jj] = perturbed_foiss[:, kk, jj_ind]
                 # compute difference in observationwise loss
-                lss[jj, kk, :] = (loss(self.model(X_test_perturbed), y_test) -
+                lss[jj_ind, kk, :] = (loss(self.model(X_test_perturbed), y_test) -
                                   loss(self.model(X_test), y_test))
 
         # return explanation object

rfi/samplers/cnflow.py
@@ -13,9 +13,9 @@ logger = logging.getLogger(__name__)
 
 
 class CNFSampler(Sampler):
-    def __init__(self, X_train):
-        super().__init__(X_train)
-        self.time_budget_s = 120
+    def __init__(self, X_train, X_val, time_budget_s=120, **kwargs):
+        super().__init__(X_train, X_val)
+        self.time_budget_s = time_budget_s
 
     def train(self, J, G, verbose=True):
 
@@ -23,11 +23,40 @@ class CNFSampler(Sampler):
         G = Sampler._to_array(G)
         super().train(J, G, verbose=verbose)
 
-        if not super()._train_J_degenerate(J, G, verbose=verbose):
-            logger.info(f'Fitting sampler for feature {j}. Time budget for CV search: {self.time_budget_s} sec')
-            cnf = ConditionalNormalisingFlowEstimator(context_size=len(G))
-            cnf.fit_by_cv(train_inputs=self.X_train[:, J], train_context=self.X_train[:, G], 
-                          time_budget_s=self.time_budget_s)
+        val_log_probs = []
+
+        for j in J:
+            j = Sampler._to_array([j])
+            if not self._train_J_degenerate(j, G, verbose=verbose):
+                logger.info(f'Fitting sampler for feature {j}. Time budget for CV search: {self.time_budget_s} sec')
+
+                # TODO CNF for multivariate J
+                cnf = ConditionalNormalisingFlowEstimator(context_size=len(G))
+                cnf.fit_by_cv(train_inputs=self.X_train[:, j], train_context=self.X_train[:, G], time_budget_s=self.time_budget_s)
+
+                def samplefunc(X_test, **kwargs):
+                    return cnf.sample(X_test[:, G], **kwargs)
+
+                self._store_samplefunc(j, G, samplefunc, verbose=verbose)
+
+                if self.X_val is not None:
+                    val_log_prob = cnf.log_prob(inputs=self.X_val[:, j], context=self.X_val[:, G]).mean()
+                    val_log_probs.append(val_log_prob)
+
+            elif self.X_val is not None:
+                val_log_probs.append(None)
+
+        if len(J) > 1:
+
             def samplefunc(X_test, **kwargs):
-                return gaussian_estimator.sample(X_test[:, G], **kwargs)
-            super()._store_samplefunc(J, G, samplefunc, verbose=verbose)
+                sampled_data = []
+                for j in J:
+                    j = Sampler._to_array([j])
+                    G_key, j_key = Sampler._to_key(G), Sampler._to_key(j)
+                    sampled_data.append(np.squeeze(self._trained_sampling_funcs[(j_key, G_key)](X_test, **kwargs)))
+                return np.stack(sampled_data, axis=-1)
+
+            self._store_samplefunc(J, G, samplefunc, verbose=verbose)
+
+        if self.X_val is not None:
+            return val_log_probs

rfi/samplers/gaussian.py
@@ -19,9 +19,9 @@ class GaussianSampler(Sampler):
         see rfi.samplers.Sampler
     """
 
-    def __init__(self, X_train):
+    def __init__(self, X_train, X_val=None, **kwargs):
         """Initialize Sampler with X_train and mask."""
-        super().__init__(X_train)
+        super().__init__(X_train, X_val)
             
 
     def train(self, J, G, verbose=True):
@@ -33,18 +33,44 @@ class GaussianSampler(Sampler):
             G: arbitrary set of variables
             verbose: printing
         """
+
         J = Sampler._to_array(J)
         G = Sampler._to_array(G)
         super().train(J, G, verbose=verbose)
 
-        if not super()._train_J_degenerate(J, G, verbose=verbose):
-            logging.debug('No degenerate case. Fitting GaussianConditionalEstimator.')
-            gaussian_estimator = GaussianConditionalEstimator()
-            gaussian_estimator.fit(train_inputs=self.X_train[:, J], train_context=self.X_train[:, G])
-            def samplefunc(X_test, **kwargs):
-                return gaussian_estimator.sample(X_test[:, G], **kwargs)
-            super()._store_samplefunc(J, G, samplefunc, verbose=verbose)
+        val_log_probs = []
 
+        for j in J:
+            j = Sampler._to_array([j])
+            if not self._train_J_degenerate(j, G, verbose=verbose):
+
+                gaussian_estimator = GaussianConditionalEstimator()
+                gaussian_estimator.fit(train_inputs=self.X_train[:, j], train_context=self.X_train[:, G])
+
+                def samplefunc(X_test, **kwargs):
+                    return gaussian_estimator.sample(X_test[:, G], **kwargs)
+
+                self._store_samplefunc(j, G, samplefunc, verbose=verbose)
+
+                if self.X_val is not None:
+                    val_log_prob = gaussian_estimator.log_prob(inputs=self.X_val[:, j], context=self.X_val[:, G]).mean()
+                    val_log_probs.append(val_log_prob)
+
+            elif self.X_val is not None:
+                val_log_probs.append(None)
+
+        if len(J) > 1:
+            def samplefunc(X_test, **kwargs):
+                sampled_data = []
+                for j in J:
+                    j = Sampler._to_array([j])
+                    G_key, j_key = Sampler._to_key(G), Sampler._to_key(j)
+                    sampled_data.append(np.squeeze(self._trained_sampling_funcs[(j_key, G_key)](X_test, **kwargs)))
+                return np.stack(sampled_data, axis=-1)
+            self._store_samplefunc(J, G, samplefunc, verbose=verbose)
+
+        if self.X_val is not None:
+            return val_log_probs
 
 
 # def train_gaussian(X_train, J, G):