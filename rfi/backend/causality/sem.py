Syncronize fit methods for CNFSampler and CNFEstimator, fix numerical issues for LinearGaussianSEM, add G(n,m) random graphj model, decrease hparam search grid for CNF

rfi/backend/causality/sem.py
@@ -263,7 +263,11 @@ class LinearGaussianNoiseSEM(StructuralEquationModel):
     def parents_conditional_distribution(self, node: str, parents_context: Dict[str, Tensor] = None) -> Distribution:
         # Only conditioning on parent nodes is possible for now
         assert set(self.model[node]['parents']) == set(parents_context.keys())
-        return self.conditional_distribution(node, parents_context)
+        linear_comb = 0.0
+        if len(self.model[node]['parents']) > 0:
+            for par in parents_context.keys():
+                linear_comb += parents_context[par] * torch.tensor(self.model[node]['coeff'][par])
+        return Normal(linear_comb, self.model[node]['noise_std'])
 
     @property
     def joint_cov(self) -> Tensor:

rfi/backend/cnf/cnf_estimator.py
@@ -191,7 +191,7 @@ class ConditionalNormalisingFlowEstimator(Flow):
         return self
 
     def fit_by_cv(self, train_inputs: Union[np.array, Tensor], train_context: Union[np.array, Tensor],
-                  hparam_grid=None, n_splits=5, resources_per_trial={"cpu": 0.5}, time_budget_s=None):
+                  hparam_grid=None, n_splits=5, resources_per_trial={"cpu": 0.5}, time_budget_s=None, num_cpus=15):
         """
         Method for hyper-parameter search for Conditional Normalizing Flow density estimator, performs K-fold cross-validation.
         After the hyper-parameter search, fits the best sem on full train dataset.
@@ -204,11 +204,12 @@ class ConditionalNormalisingFlowEstimator(Flow):
             n_splits: Number of splits for K-Fold cross-validation
             resources_per_trial: Ray tune parameter
             time_budget_s: Total time budget (Ray tune parameter)
+            num_cpus: Number of CPUs to employ
 
         Returns: self
         """
 
-        ray.init(logging_level=logging.WARN)
+        ray.init(logging_level=logging.WARN, num_cpus=num_cpus)
         logger.info(f'Start fitting, using {n_splits}-fold split, time budget: {time_budget_s}')
 
         if hparam_grid is None: