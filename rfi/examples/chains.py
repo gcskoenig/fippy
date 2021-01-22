fa070a47c0beee795f05f2268f91c95f7cb5641e
gcskoenig <gunnar.koenig@pm.me>
2021-01-22 12:46:53 +0100

    added two examples

--- rfi/examples/chains.py
+++ rfi/examples/chains.py
@@ -50,3 +50,62 @@ chain2 = SyntheticExample(
         noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'x3': sigma_low, 'x4': sigma_high, 'y': sigma_medium}
     )
 )
+
+"""
+EXAMPLE 2: CHAIN with multiple paths
+
+Order: X_1, X_2, X_3, X_4, Y
+"""
+chain2 = SyntheticExample(
+    name='chain2',
+    sem=LinearGaussianNoiseSEM(
+        dag=DirectedAcyclicGraph(
+            adjacency_matrix=np.array([[0, 1, 0, 1, 0],
+                                       [0, 0, 1, 0, 0],
+                                       [0, 0, 0, 0, 1],
+                                       [0, 0, 0, 0, 1],
+                                       [0, 0, 0, 0, 0]]),
+            var_names=['x1', 'x2', 'x3', 'x4', 'y']
+        ),
+        coeff_dict={'x2': {'x1': 1.0}, 'x3': {'x2': 1.0}, 'x4': {'x1': 1.0}, 'y': {'x3': 1.0, 'x4': 1.0}},
+        noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'x3': sigma_low, 'x4': sigma_high, 'y': sigma_medium}
+    )
+)
+
+"""
+EXAMPLE 3: Figure 1 from II paper
+
+Order: X_1, X_2, Y
+"""
+ii_audit = SyntheticExample(
+    name='ii-model-audit',
+    sem=LinearGaussianNoiseSEM(
+        dag=DirectedAcyclicGraph(
+            adjacency_matrix=np.array([[0, 1, 1],
+                                       [0, 0, 0],
+                                       [0, 0, 0]]),
+            var_names=['x1', 'x2', 'y']
+        ),
+        coeff_dict={'x2': {'x1': 1.0}, 'y': {'x1': 1.0}},
+        noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'y': sigma_low}
+    )
+)
+
+"""
+EXAMPLE 4: Figure 2 from II paper
+
+Order: X_1, X_2, Y
+"""
+ii_inference = SyntheticExample(
+    name='ii-model-audit',
+    sem=LinearGaussianNoiseSEM(
+        dag=DirectedAcyclicGraph(
+            adjacency_matrix=np.array([[0, 1, 0],
+                                       [0, 0, 1],
+                                       [0, 0, 0]]),
+            var_names=['x1', 'x2', 'y']
+        ),
+        coeff_dict={'x2': {'x1': 1.0}, 'y': {'x2': 1.0}},
+        noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'y': sigma_low}
+    )
+)
