fixed sage computation

rfi/examples/chains.py
@@ -7,6 +7,7 @@ from rfi.examples import SyntheticExample
 sigma_low = 0.3
 sigma_medium = .5
 sigma_high = 1
+sigma_veryhigh = 1.5
 
 
 """
@@ -87,7 +88,7 @@ ii_audit = SyntheticExample(
             var_names=['x1', 'x2', 'y']
         ),
         coeff_dict={'x2': {'x1': 1.0}, 'y': {'x1': 1.0}},
-        noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'y': sigma_low}
+        noise_std_dict={'x1': sigma_medium, 'x2': sigma_medium, 'y': sigma_low}
     )
 )
 
@@ -97,7 +98,7 @@ EXAMPLE 4: Figure 2 from II paper
 Order: X_1, X_2, Y
 """
 ii_inference = SyntheticExample(
-    name='ii-model-audit',
+    name='ii-inference',
     sem=LinearGaussianNoiseSEM(
         dag=DirectedAcyclicGraph(
             adjacency_matrix=np.array([[0, 1, 0],
@@ -107,5 +108,26 @@ ii_inference = SyntheticExample(
         ),
         coeff_dict={'x2': {'x1': 1.0}, 'y': {'x2': 1.0}},
         noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'y': sigma_low}
+    )
+)
+
+"""
+EXAMPLE 4: Figure 2 from II paper
+
+Order: X_1, X_2, Y
+"""
+ii_inference_large = SyntheticExample(
+    name='ii-inference-large',
+    sem=LinearGaussianNoiseSEM(
+        dag=DirectedAcyclicGraph(
+            adjacency_matrix=np.array([[0, 1, 0, 0, 0],
+                                       [0, 0, 0, 0, 1],
+                                       [0, 0, 0, 0, 1],
+                                       [0, 0, 1, 0, 0],
+                                       [0, 0, 0, 0, 0]]),
+            var_names=['x1', 'x2', 'x3', 'x4', 'y']
+        ),
+        coeff_dict={'x2': {'x1': 1.0}, 'x3': {'x4': 1.0}, 'y': {'x2': 1.0, 'x3':1.0}},
+        noise_std_dict={'x1': sigma_high, 'x2': sigma_high, 'x3': sigma_high, 'x4': sigma_high,'y': sigma_low}
     )
 )

rfi/explanation/explanation.py
@@ -57,5 +57,5 @@ class Explanation:
         """
         return np.std(np.mean(np.mean(self.lss, axis=3), axis=2), axis=1)
 
-    def barplot(self, ax=None):
-        return _barplot.fi_hbarplot(self, ax=ax)
+    def barplot(self, ax=None, figsize=None):
+        return _barplot.fi_hbarplot(self, ax=ax, figsize=figsize)