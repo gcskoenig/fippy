1936f152548d81d70033e9bb3b4a7135ca19b97e
Valentyn Melnychuk <v.melnychuk@campus.lmu.de>
2021-02-08 17:10:42 +0100

    GoF testing KL-div decimals, number of threads for Gitlab runner

--- tests/test_goodnesses_of_fit.py
+++ tests/test_goodnesses_of_fit.py
@@ -1,13 +1,11 @@
 import logging
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
 from typing import Union
 
 from rfi.backend.goodness_of_fit import conditional_js_divergence, conditional_kl_divergence, conditional_hellinger_distance
 from rfi.backend.causality import DirectedAcyclicGraph, PostNonLinearLaplaceSEM, PostNonLinearMultiplicativeHalfNormalSEM, \
     LinearGaussianNoiseSEM, RandomGPGaussianNoiseSEM, StructuralEquationModel
-from rfi.examples import SyntheticExample
 from rfi.backend.gaussian import GaussianConditionalEstimator
 from rfi.backend.cnf import NormalisingFlowEstimator
 
@@ -43,12 +41,13 @@ class TestConditionalGoF:
 
     @staticmethod
     def assert_metrics(test_df1: pd.DataFrame, test_df2: pd.DataFrame, sem: StructuralEquationModel,
-                       estimator: Union[GaussianConditionalEstimator, NormalisingFlowEstimator], metric_func: callable):
+                       estimator: Union[GaussianConditionalEstimator, NormalisingFlowEstimator], metric_func: callable,
+                       assert_decimals: int):
         # Calculating metrics twice and checking equality (mb_cond_distributions could be different)
         np.testing.assert_almost_equal(
             metric_func(estimator, sem, TARGET_VAR, CONTEXT_VARS, GOF_ARGS, 'true_markov_blanket', test_df1),
             metric_func(estimator, sem, TARGET_VAR, CONTEXT_VARS, GOF_ARGS, 'true_markov_blanket', test_df2),
-            ASSERT_DECIMAL
+            assert_decimals
         )
 
     def test_linear_gaussian_noise_sem_conditional_kl_divergence(self):
@@ -56,21 +55,21 @@ class TestConditionalGoF:
         train_df, test_df1, test_df2 = self.get_train_test_dfs(sem)
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence, ASSERT_DECIMAL - 1)
 
     def test_linear_gaussian_noise_sem_conditional_js_divergence(self):
         sem = LinearGaussianNoiseSEM(DAG, seed=SEED)
         train_df, test_df1, test_df2 = self.get_train_test_dfs(sem)
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence, ASSERT_DECIMAL)
 
     def test_linear_gaussian_noise_sem_conditional_hellinger_distance(self):
         sem = LinearGaussianNoiseSEM(DAG, seed=SEED)
         train_df, test_df1, test_df2 = self.get_train_test_dfs(sem)
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance, ASSERT_DECIMAL)
 
     def test_randomgp_gaussian_noise_sem_conditional_kl_divergence(self):
         sem = RandomGPGaussianNoiseSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -78,7 +77,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence, ASSERT_DECIMAL - 1)
 
     def test_randomgp_gaussian_noise_sem_conditional_js_divergence(self):
         sem = RandomGPGaussianNoiseSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -86,7 +85,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence, ASSERT_DECIMAL)
 
     def test_randomgp_gaussian_noise_sem_conditional_hellinger_distance(self):
         sem = RandomGPGaussianNoiseSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -94,7 +93,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance, ASSERT_DECIMAL)
 
     def test_post_nonlinear_laplace_sem_conditional_kl_divergence(self):
         sem = PostNonLinearLaplaceSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -102,7 +101,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence, ASSERT_DECIMAL - 1)
 
     def test_post_nonlinear_laplace_sem_conditional_js_divergence(self):
         sem = PostNonLinearLaplaceSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -110,7 +109,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence, ASSERT_DECIMAL)
 
     def test_post_nonlinear_laplace_sem_conditional_hellinger_distance(self):
         sem = PostNonLinearLaplaceSEM(DAG, seed=SEED, interpolation_switch=2 * SAMPLE_SIZE)
@@ -118,7 +117,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance, ASSERT_DECIMAL)
 
     def test_post_nonlinear_multiplicative_half_normal_sem_conditional_kl_divergence(self):
         sem = PostNonLinearMultiplicativeHalfNormalSEM(DAG, seed=SEED)
@@ -126,7 +125,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_kl_divergence, ASSERT_DECIMAL - 1)
 
     def test_post_nonlinear_multiplicative_half_normal_sem_conditional_js_divergence(self):
         sem = PostNonLinearMultiplicativeHalfNormalSEM(DAG, seed=SEED)
@@ -134,7 +133,7 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_js_divergence, ASSERT_DECIMAL)
 
     def test_post_nonlinear_multiplicative_half_normal_sem_conditional_hellinger_distance(self):
         sem = PostNonLinearMultiplicativeHalfNormalSEM(DAG, seed=SEED)
@@ -142,4 +141,4 @@ class TestConditionalGoF:
 
         for estimator in ESTIMATORS:
             estimator.fit(train_inputs=train_df[[TARGET_VAR]].values, train_context=train_df[CONTEXT_VARS].values)
-            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance)
+            self.assert_metrics(test_df1, test_df2, sem, estimator, conditional_hellinger_distance, ASSERT_DECIMAL)
