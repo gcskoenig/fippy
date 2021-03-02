9e0bcee475bf5c6c4f65c3d0d2afd90de41001fa
gcskoenig <gunnar.koenig@pm.me>
2021-03-02 20:04:08 +0100

    unstable

--- rfi/samplers/cnflow.py
+++ rfi/samplers/cnflow.py
@@ -12,7 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 class CNFSampler(Sampler):
-    def __init__(self, X_train, fit_method='fit_by_cv', fit_params={'time_budget_s': None}, **kwargs):
+    def __init__(self, X_train, fit_method='fit_by_cv',
+                 fit_params={'time_budget_s': None}, **kwargs):
         super().__init__(X_train, **kwargs)
         self.fit_method = fit_method
         self.fit_params = fit_params if fit_params is not None else {}
@@ -26,35 +27,49 @@ class CNFSampler(Sampler):
         if not self._train_J_degenerate(J, G, verbose=verbose):
 
             if not set(J).isdisjoint(self.cat_inputs) and len(J) > 1:
-                raise NotImplementedError('Multiple categorical or mixed variables sampling is not supported.')
+                raise NotImplementedError('Multiple categorical or mixed '
+                                          'variables sampling is not '
+                                          'supported.')
 
             # TODO(valik): adjust to loading data from data frame using
             # J, G lists of columnnames
-            train_inputs = self.X_train[:, J]
-            train_context = self.X_train[:, G]
+            train_inputs = self.X_train[Sampler._order_fset(J)].to_numpy()
+            train_context = self.X_train[Sampler._order_fset(G)].to_numpy()
 
             # Categorical variables in context
             cat_context = None
             if not set(G).isdisjoint(self.cat_inputs):
-                G_cat = Sampler._to_array(list(set(G).intersection(self.cat_inputs)))
+                G_cat = list(set(G).intersection(self.cat_inputs))
                 cat_context = search_nonsorted(G, G_cat)
-                logger.info(f'One hot encoding following context features: {G_cat}')
+                logger.info('One hot encoding following '
+                            'context features: '
+                            '{}'.format(G_cat))
 
             # Categorical variable as input
             if not set(J).isdisjoint(self.cat_inputs):
                 logger.info(f'One hot encoding following inputs features: {J}')
-                logger.info(f'Fitting categorical sampler for features {J}. Fitting method: {self.fit_method}. '
+                logger.info(f'Fitting categorical sampler for features {J}. '
+                            'Fitting method: {self.fit_method}. '
                             f'Fitting parameters: {self.fit_params}')
-                model = CategoricalEstimator(context_size=train_context.shape[1], cat_context=cat_context, **self.fit_params)
+                context_size = train_context.shape[1]
+                model = CategoricalEstimator(context_size=context_size,
+                                             cat_context=cat_context,
+                                             **self.fit_params)
             # Continuous variable as input
             else:
-                logger.info(f'Fitting continuous sampler for features {J}. Fitting method: {self.fit_method}. '
+                logger.info(f'Fitting continuous sampler for features {J}. '
+                            'Fitting method: {self.fit_method}. '
                             f'Fitting parameters: {self.fit_params}')
-                model = NormalisingFlowEstimator(inputs_size=len(J), context_size=train_context.shape[1], cat_context=cat_context,
+
+                model = NormalisingFlowEstimator(inputs_size=len(J),
+                                                 context_size=context_size,
+                                                 cat_context=cat_context,
                                                  **self.fit_params)
 
             # Fitting a sampler
-            getattr(model, self.fit_method)(train_inputs=train_inputs, train_context=train_context, **self.fit_params)
+            getattr(model, self.fit_method)(train_inputs=train_inputs,
+                                            train_context=train_context,
+                                            **self.fit_params)
 
             def samplefunc(eval_context, **kwargs):
                 return model.sample(eval_context, **kwargs)
