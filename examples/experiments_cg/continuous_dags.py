1859a405bb86e58ffc51a859a214b5c8040fba90
ChristophLuther <cph.luther@gmail.com>
2021-10-06 17:55:53 +0200

    minor changes

--- examples/experiments_cg/continuous_dags.py
+++ examples/experiments_cg/continuous_dags.py
@@ -104,7 +104,7 @@ def main(args):
 
     # split data for train and test purpose
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.1, random_state=42
+        X, y, test_size=0.2, random_state=42
     )
 
     try:
