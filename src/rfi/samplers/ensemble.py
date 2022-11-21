e6c8699b20c9c229e24906dbbc644b0b70267cd5
gcskoenig <gunnar.koenig@pm.me>
2022-11-21 14:57:09 +0100

    added RF for cont features

--- src/rfi/samplers/ensemble.py
+++ src/rfi/samplers/ensemble.py
@@ -5,7 +5,7 @@ only works for univariate targets.
 import numpy as np
 import pandas as pd
 from rfi.samplers.sampler import Sampler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
 import torch
 import category_encoders as ce
@@ -78,6 +78,64 @@ class UnivRFSampler(Sampler):
                     pred_proba = torch.tensor(model.predict_proba(X_eval_enc))
                     sample = torch.multinomial(pred_proba, 1).numpy().flatten()
                     sample = model.classes_[sample]
+                    arrs.append(sample.reshape(1, -1, len(J)))
+                res = np.concatenate(arrs, axis=0)
+                res = np.swapaxes(res, 0, 1)
+                return res
+
+            self._store_samplefunc(J, G, samplefunc, verbose=verbose)
+
+
+class ContUnivRFSampler(Sampler):
+    """
+    Simple sampling from observed data
+    Attributes:
+        see rfi.samplers.Sampler
+    """
+
+    def __init__(self, X_train, **kwargs):
+        """Initialize Sampler with X_train and mask."""
+        super().__init__(X_train, **kwargs)
+
+    def train(self, J, G, verbose=True):
+        """
+        Trains sampler using dataset to resample variable jj relative to G.
+        Args:
+            J: features of interest
+            G: arbitrary set of variables
+            verbose: printing
+        """
+        assert len(J) == 1
+        J = Sampler._to_array(list(J))
+        G = Sampler._to_array(list(G))
+        super().train(J, G, verbose=verbose)
+
+        JuG = list(set(J).union(G))
+
+        if not self._train_J_degenerate(J, G, verbose=verbose):
+            # TODO assert that target variable is continuous
+
+            rf = RandomForestRegressor()  # Instantiate the grid search model
+            # rf_random = RandomizedSearchCV(estimator=rf, param_distributions=param_grid,
+            #                                n_iter=100, verbose=0,
+            #                                n_jobs=-1, scoring='neg_log_loss')  # Fit the random search model
+            X_train_G, X_train_J = self.X_train[Sampler._order_fset(G)], self.X_train[J[0]]
+            enc_G = ce.OneHotEncoder()
+            enc_G.fit(X_train_G)
+            X_train_G_enc = enc_G.transform(X_train_G)
+            rf.fit(X_train_G_enc, X_train_J)
+            model = rf  # _random.best_estimator_
+            resids = X_train_J - rf.predict(X_train_G_enc)
+
+            def samplefunc(eval_context, num_samples=1, **kwargs):
+                arrs = []
+                for snr in range(num_samples):
+                    X_eval = pd.DataFrame(data=eval_context, columns=Sampler._order_fset(G))
+                    X_eval_enc = enc_G.transform(X_eval)
+
+                    # TODO: use the model to sample the target variable
+                    pred = torch.tensor(model.predict(X_eval_enc))
+                    sample = pred + np.random.choice(resids, pred.shape[0])
                     arrs.append(sample.reshape(1, -1, len(J)))
                 res = np.concatenate(arrs, axis=0)
                 res = np.swapaxes(res, 0, 1)
